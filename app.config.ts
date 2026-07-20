5016a1fa2f3511f7de8514dc07878b2bd27d9e33
vxmpie <247774481+swnst@users.noreply.github.com>
2026-07-20 20:20:18 +0700

    upd

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from '@tanstack/react-start/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     server: {
         preset: 'vercel',
+    },
+    vite: {
+        plugins: [
+            tailwindcss(),
+            tsconfigPaths(),
+        ],
     },
 });
