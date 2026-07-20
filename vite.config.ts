131238512e724e959b925459e53dda0259761e7f
vxmpie <247774481+swnst@users.noreply.github.com>
2026-07-20 19:20:54 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-﻿import { defineConfig } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { tanstackStart } from '@tanstack/react-start/plugin/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
+    TanStackRouterVite(),
     react(),
     tailwindcss(),
     tsconfigPaths(),
