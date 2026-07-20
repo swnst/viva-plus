d087a7db4550f74c2f0fcdbe1419c74e82a9a14a
vxmpie <247774481+swnst@users.noreply.github.com>
2026-07-20 19:35:09 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    TanStackRouterVite(),
+    tanstackStart({
+      server: {
+        preset: 'vercel',
+      }
+    }),
     react(),
     tailwindcss(),
     tsconfigPaths(),
